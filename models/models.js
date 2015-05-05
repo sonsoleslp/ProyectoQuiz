4f10692eee73342bfbcbaf77124034bdeafe1b8c
sonsoleslp <sonsoles.lopez.pernas@alumnos.upm.es>
2015-05-06 00:54:46 +0200

    pruebadb

--- models/models.js
+++ models/models.js
@@ -1,51 +1,40 @@
 var path = require('path');
 
-// Postgres DATABASE_URL = postgres://user:passwd@host:port/database
-// SQLite   DATABASE_URL = sqlite://:@:/
 var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
-var DB_name  = (url[6]||null);
-var user     = (url[2]||null);
-var pwd      = (url[3]||null);
-var protocol = (url[1]||null);
+var DB_name = (url[6]||null);
+var user = (url[2]||null);
+var pwd = (url[3]||null);
+var protocol  = (url[1]||null);
 var dialect  = (url[1]||null);
-var port     = (url[5]||null);
-var host     = (url[4]||null);
+var port  = (url[5]||null);
+var host  = (url[4]||null);
 var storage  = process.env.DATABASE_STORAGE;
 
-// Cargar Modelo ORM
 var Sequelize = require('sequelize');
-
-// Usar BBDD SQLite
-var sequelize = new Sequelize(DB_name, user, pwd, 
-  { dialect:  protocol,
-    protocol: protocol,
-    port:     port,
-    host:     host,
-    storage:  storage,  // solo SQLite (.env)
-    omitNull: true      // solo Postgres
-  }
-  );
-
-// Importar definicion de la tabla Quiz en quiz.js
-var Quiz = sequelize.import(path.join(__dirname,'quiz'));
-
+var sequelize = new Sequelize(DB_name,user,pwd,
+	{ dialect:protocol,
+ 	  protocol:protocol,
+ 	  port:port,
+ 	  host:host,
+ 	  storage:storage,
+ 	  omitNull:true
+ 	 });
+var quiz_path = path.join(__dirname,'quiz');
+var Quiz = sequelize.import(quiz_path);
 exports.Quiz = Quiz;
-
-// sequelize.sync() inicializa tabla de preguntas en DB
-
 sequelize.sync().then(function(){
-  // then(..) ejecuta el manejador una vez creada la tabla
-  Quiz.count().then(function (count){
-    	if(count === 0) {   // La tabla se inicializa solo si está vacía
-    		Quiz.create({
-          pregunta: 'Capital de Italia',
-          respuesta: 'Roma'
-        });
-        Quiz.create({
-          pregunta: 'Capital de Portugal',
-          respuesta: 'Lisboa'
-        })
-        .then(function(){console.log('Base de datos inicializada')});
-      };
-    });
+	Quiz.count().then(function(count){
+		if(count === 0) {
+			Quiz.create ({
+				pregunta:'Capital de Italia',
+				respuesta:'Roma'
+			});
+
+			Quiz.create ({
+				pregunta:'Capital de Portugal',
+				respuesta:'Lisboa'
+			}).then(function(){console.log('Base de datos inicializada')});
+		};
+	});
+
 });

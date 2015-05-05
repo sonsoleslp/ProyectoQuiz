83bf71e013e93ecc200af9f0b71ece480e5f6b54
sonsoleslp <sonsoles.lopez.pernas@alumnos.upm.es>
2015-05-05 23:53:48 +0200

    pruebadb

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,60 +1,36 @@
 var models = require('../models/models.js');
 
-
-
-
-exports.index=function(req,res){
-	models.Quiz.findAll().then(function(quizes) {
-		res.render('quizes/index.ejs', {quizes:quizes});
-	})
-};
-
-
-exports.show=function(req,res){
+// GET /quiz/:id
+exports.show = function (req,res){
 	models.Quiz.find(req.params.quizId).then(function(quiz){
 		res.render('quizes/show',{quiz: quiz});
-
-	})
+	})	
 };
 
+// GET /quizes/:id/answer
 exports.answer = function(req,res){
 	models.Quiz.find(req.params.quizId).then(function(quiz){
-		if(req.query.respuesta === quiz.respuesta) {
-			res.render('quizes/answer',{quiz:quiz, respuesta:'Correcto' });
+		if (req.query.respuesta === quiz.respuesta){
+			res.render('quizes/answer',
+				{quiz: quiz, respuesta: 'Correcto'});
 		}else{
-		 	res.render('quizes/answer', {quiz:quiz, respuesta:'Incorrecto'});
+			res.render('quizes/answer',
+				{quiz: quiz, respuesta: 'Incorrecto'});
 		}
 	})
 };
 
-
-/*
-exports.question = function(req, res){
-	models.Quiz.findAll().success(function(quiz){
-		res.render('quizes/question',{pregunta:quiz[0].pregunta});
- })
-};
-
-
-exports.answer = function(req,res){
-	models.Quiz.findAll().success(function(quiz){
-		if(req.query.respuesta === quiz[0].respuesta) {
-			res.render('quizes/answer', {respuesta: 'Correcto'});
-		}else{
-			res.render('quizes/answer', {respuesta: 'Incorrecto'});
-		}
-	})
-};
-
-*/
-/*//GET /quizes/question
-exports.question = function(req, res){
-	res.render('quizes/question',{pregunta:'Capital de Italia'});
-};
-exports.answer = function(req,res){
-	if(req.query.respuesta==='Roma'){
-		res.render('quizes/answer', {respuesta:'Correcto'});
+// GET /quizes
+exports.index = function(req,res){
+	console.log(req.query.search);
+	if (req.query.search !== undefined){
+		var search= '%' +(String(req.query.search)).replace(/\s/g,"%")+'%';
+		models.Quiz.findAll({where: ["pregunta like ?",search]}).then(function(quizes){
+			res.render('quizes/index.ejs',{quizes: quizes});
+		})
 	}else{
-		res.render('quizes/answer', {respuesta: 'Incorrecto'});
+		models.Quiz.findAll().then(function(quizes){
+			res.render('quizes/index.ejs',{quizes: quizes});
+		})
 	}
-};*/
+};

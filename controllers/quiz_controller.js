98b91f8399a17c44ab285a5d1eed026bac7e2aad
sonsoleslp <sonsoles.lopez.pernas@alumnos.upm.es>
2015-05-05 23:00:10 +0200

    pruebadb

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,4 +1,24 @@
-//GET /quizes/question
+var models = require('../models/models.js');
+
+exports.question = function(req, res){
+	models.Quiz.findAll().success(function(quiz){
+		res.render('quizes/question',{pregunta:quiz[0].pregunta});
+ })
+};
+
+
+exports.answer = function(req,res){
+	models.Quiz.findAll().success(function(quiz){
+		if(req.query.respuesta === quiz[0].respuesta) {
+			res.render('quizes/answer', {respuesta: 'Correcto'});
+		}else{
+			res.render('quizes/answer', {respuesta: 'Incorrecto'});
+		}
+	})
+};
+
+
+/*//GET /quizes/question
 exports.question = function(req, res){
 	res.render('quizes/question',{pregunta:'Capital de Italia'});
 };
@@ -8,4 +28,4 @@ exports.answer = function(req,res){
 	}else{
 		res.render('quizes/answer', {respuesta: 'Incorrecto'});
 	}
-};
+};*/

8ebe0dbd8457f19585f9b3681240bf66beb00fd2
sonsoleslp <sonsoles.lopez.pernas@alumnos.upm.es>
2015-05-10 22:47:49 +0200

    Entrega 2.4

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,16 +1,43 @@
 var models = require('../models/models.js');
 
+
+exports.load = function(req,res,next,quizId) {
+	models.Quiz.find(quizId).then(
+			function(quiz) {
+				if(quiz){
+					req.quiz=quiz; 	next();
+				}else{ next(new Error('No existe quizId=' + quizId)); }
+			}).catch(function(error){next(error);});
+};
+
+exports.index = function(req,res){
+	console.log(req.query.search);
+	var buscar =req.query.search ;
+	if (buscar== undefined){
+
+		models.Quiz.findAll().then(function(quizes){
+			res.render('quizes/index.ejs',{quizes: quizes}).catch(function(error){next(error)});
+		
+		})
+	}else{
+		var search= '%' +(String(buscar)).replace(/\s/g,"%")+'%';
+		models.Quiz.findAll({where: ["pregunta like ?",search], order: ['pregunta']}).then(function(quizes){
+			res.render('quizes/index.ejs',{quizes: quizes});
+		}).catch(function(error){next(error);});
+	}
+};
+
 // GET /quiz/:id
 exports.show = function (req,res){
 	models.Quiz.find(req.params.quizId).then(function(quiz){
-		res.render('quizes/show',{quiz: quiz});})	
+		res.render('quizes/show',{quiz: req.quiz});})	
 	
 };
 
 // GET /quizes/:id/answer
 exports.answer = function(req,res){
 	models.Quiz.find(req.params.quizId).then(function(quiz){
-		if (req.query.respuesta === quiz.respuesta){
+		if (req.query.respuesta === req.quiz.respuesta){
 			res.render('quizes/answer',
 				{quiz: quiz, respuesta: 'Correcto'});
 		}else{
@@ -21,19 +48,3 @@ exports.answer = function(req,res){
 };
 
 
-exports.index = function(req,res){
-	console.log(req.query.search);
-	var buscar =req.query.search 
-	if (buscar== undefined){
-
-		models.Quiz.findAll().then(function(quizes){
-			res.render('quizes/index.ejs',{quizes: quizes});
-		
-		})
-	}else{
-		var search= '%' +(String(buscar)).replace(/\s/g,"%")+'%';
-		models.Quiz.findAll({where: ["pregunta like ?",search], order: ['pregunta']}).then(function(quizes){
-			res.render('quizes/index.ejs',{quizes: quizes});
-		})
-	}
-};

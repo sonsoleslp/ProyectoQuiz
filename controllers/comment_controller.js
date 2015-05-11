83ce5de90a7595b4e7aa2fd46fffc74b371bbedf
sonsoleslp <sonsoles.lopez.pernas@alumnos.upm.es>
2015-05-11 12:56:09 +0200

    Idempotencia

---
+++ controllers/comment_controller.js
@@ -0,0 +1,20 @@
+var models = require('../models/models.js');
+// GET /quizes/:quizId/comments/new
+exports.new = function(req,res){
+	res.render('comments/new.ejs', {quizid: req.params.quizId, errors:[]});
+
+};
+
+exports.create = function(req,res){
+	var comment = models.Comment.build(
+		{texto: req.body.comment.texto, 
+			QuizId: req.params.quizId});
+
+comment.validate().then(function(err){
+	if(err){
+		res.render('comments/new.ejs',{comment:comment, errors: err.errors});	
+	} else { comment
+		.save()
+		.then( function(){res.redirect('/quizes/'+req.params.quizId)})}
+	
+}).catch(function(error){next(error)});};

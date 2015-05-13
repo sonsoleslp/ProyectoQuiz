bf1998ca4b19db76022c4378558eaa4eedc55903
sonsoleslp <sonsoles.lopez.pernas@alumnos.upm.es>
2015-05-13 22:31:57 +0200

    Autologout

--- app.js
+++ app.js
@@ -32,16 +32,39 @@ app.use(session());
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+
+
 //Helpers dinámicos:
 app.use(function(req,res,next){
     //guardar path en session.redir para despues de login
-    if (!req.path.match(/\/login|\/logout/)){
+    if (!req.path.match(/\/login|\/logout|\/user/)){
         req.session.redir = req.path;
     }
     //hacer visible req.session en las vistas
     res.locals.session = req.session;
+
     next();
 });
+
+//Logout automático
+app.use(function(req,res,next){
+    if (req.session.user){
+        if(req.session.user.inicio){
+            console.log("user: "+req.session.user.inicio+" \n");
+            console.log(new Date().getTime()+" \n");
+            if((new Date().getTime()-req.session.user.inicio)>120000){
+                req.session.user = undefined;
+               
+            }else {req.session.user.inicio = new Date().getTime();}
+           
+        } else {req.session.user.inicio = new Date().getTime();}
+
+    }
+    next();
+});
+
 
 
 
